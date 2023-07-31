426e5a68f7bd33ae8b31c738baa2e6f273904e72
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-30 22:39:21 -0400

    Selecting data

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -30,17 +30,17 @@ foreach (var animal in animals.OrderByDescending(animal => animal))
     Console.WriteLine(animal);
 }
 
-var persons = new List<Person>
+/*var persons = new List<Person>
 {
     new Person("John", "Deer"),
     new Person("John1", "Deer2"),
     new Person("John2", "Deer2"),
-};
+};*/
 
-foreach (var person in persons.OrderBy(person => person.FirstName).ThenBy(person => person.FirstName))
+/*foreach (var person in persons2.OrderBy(person => person.FirstName).ThenBy(person => person.FirstName))
 {
     Console.WriteLine($"{person.FirstName} {person.LastName}");
-}
+}*/
 
 animals.Reverse();
 foreach(var animal in animals)
@@ -48,4 +48,29 @@ foreach(var animal in animals)
     Console.WriteLine(animal);
 }
 
-record Person (string FirstName, string LastName);
+var persons2 = new List<Person>
+{
+    new Person("John", "Miller", 27),
+    new Person("John1", "Miller1", 28),
+    new Person("John2", "Miller2", 31),
+};
+
+var lastNames = persons2.Select(person => person.LastName);
+
+foreach(var lastName in lastNames)
+{
+    Console.WriteLine(lastName);
+}
+
+var ageBetween20and30 = persons2.Where(person => person.Age >= 20 && person.Age <= 30)
+                                .Select(person => person.Age);
+
+foreach (var age in ageBetween20and30)
+{
+    Console.WriteLine(age);
+}
+
+var firstPerson = persons2.First();
+Console.WriteLine(firstPerson.FirstName);
+
+record Person (string FirstName, string LastName, int Age);

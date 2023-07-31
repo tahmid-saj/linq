b9699e1f87c125a23481e69a2788522bfead8caf
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-30 22:26:46 -0400

    Ordering data

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -16,3 +16,36 @@ static bool FilterEvenNumbers(int num)
 {
     return num % 2 == 0;
 }
+
+// Ordering
+var animals = new List<string> { "Deer", "Sheep", "Cat", "Dog", "Bull", "Donkey", "Goat" };
+
+foreach (var animal in animals.OrderBy(animal => animal))
+{
+    Console.WriteLine(animal);
+}
+
+foreach (var animal in animals.OrderByDescending(animal => animal))
+{
+    Console.WriteLine(animal);
+}
+
+var persons = new List<Person>
+{
+    new Person("John", "Deer"),
+    new Person("John1", "Deer2"),
+    new Person("John2", "Deer2"),
+};
+
+foreach (var person in persons.OrderBy(person => person.FirstName).ThenBy(person => person.FirstName))
+{
+    Console.WriteLine($"{person.FirstName} {person.LastName}");
+}
+
+animals.Reverse();
+foreach(var animal in animals)
+{
+    Console.WriteLine(animal);
+}
+
+record Person (string FirstName, string LastName);

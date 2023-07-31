977bfafc10178a90de5cf354a743ea2d4cefc5ce
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-30 22:13:23 -0400

    Filtering data

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+var numbers = new List<int> { 0, 1, 2, 3, 4, 5, 6 };
+
+var evenNumbers = numbers.Where(FilterEvenNumbers).ToList();
+
+numbers.Add(7);
+numbers.Add(8);
+
+foreach (var number in evenNumbers)
+{
+    Console.WriteLine(number);
+}
+
+static bool FilterEvenNumbers(int num)
+{
+    return num % 2 == 0;
+}
